992f10d0e101c20ff28b371c0a9037e11fc3d4b8
Composed Thoughts ♪ <167056918+ExcellentAlt@users.noreply.github.com>
2024-04-15 19:04:59 -0400

    n64 hates me so much

--- defines.mk
+++ defines.mk
@@ -15,7 +15,7 @@ RUMBLE_FEEDBACK ?= 1
 # Disable no drawing distance by default
 NODRAWINGDISTANCE ?= 0
 # Enable Goddard (Mario Face)
-GODDARD_MFACE ?= 1
+GODDARD_MFACE ?= 0
 # Kaze MOP Objects Port, disabled by default
 PORT_MOP_OBJS ?= 0
 # Enable level vanilla checks
